7215c9ce894ab9a783e4dbe05c8e7f53ab3bdfa7
jfsenechal <jfsenechal@gmail.com>
2024-05-06 13:49:10 +0200

    fix path wp

--- ConseilConstantes.php
+++ ConseilConstantes.php
@@ -6,6 +6,6 @@ namespace AcMarche\Conseil;
 
 class ConseilConstantes
 {
-    public const ORDRE_DIRECTORY = '/var/www/wordpress/wp-content/uploads/conseil/ordre/';
-    public const PV_DIRECTORY = '/var/www/wordpress/wp-content/uploads/conseil/pv/';
+    public const ORDRE_DIRECTORY = '/var/www/marchebe/wp-content/uploads/conseil/ordre/';
+    public const PV_DIRECTORY = '/var/www/marchebe/wp-content/uploads/conseil/pv/';
 }
